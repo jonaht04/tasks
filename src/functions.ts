0367f4c0533816c9fc9e1d4a841d4aea26b1f316
jonaht04 <jonaht@udel.edu>
2024-02-20 17:51:26 -0500

    added last two functions. yet to test

--- src/functions.ts
+++ src/functions.ts
@@ -40,7 +40,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.charAt(-1) == "?";
 }
 
 /**
@@ -49,5 +49,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toUpperCase() === "YES") {
+        return true;
+    }
+    if (word.toUpperCase() === "NO") {
+        return false;
+    }
+    return null;
 }
