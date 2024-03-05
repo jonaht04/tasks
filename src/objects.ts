80bea524ddbb3cfcc448d2d3c13a981205f78ebc
jonaht04 <jonaht@udel.edu>
2024-03-04 19:17:39 -0500

    duplicate Question finished

--- src/objects.ts
+++ src/objects.ts
@@ -120,7 +120,12 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    const newQuestion = {
+        ...oldQuestion,
+        name: "Copy of " + oldQuestion.name,
+        published: false
+    };
+    return newQuestion;
 }
 
 /**
