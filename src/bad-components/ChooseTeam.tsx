1a6f2396a7e88ced1376bf51a2f1b022704b6596
jonaht04 <jonaht@udel.edu>
2024-03-17 13:30:22 -0400

    First two task 9 functions complete

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -14,18 +14,26 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
+    function chooseMember(option: string) {
         /*
         if (!team.includes(newMember)) {
             team.push(newMember);
         }
         */
+
+        if (!team.includes(option)) {
+            const NewTeam = [...team, option];
+            setTeam(NewTeam);
+            return;
+        }
     }
 
     function clearTeam() {
         /*
-        team = [];
-        */
+        team = []
+     */
+        const newTeam: React.SetStateAction<string[]> = [];
+        setTeam(newTeam);
     }
 
     return (
@@ -36,7 +44,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
